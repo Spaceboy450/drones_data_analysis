444079a0afa38eece54e43bcf5e1f9ba9cb5e225
Alexander <asro@edu.hse.ru>
2025-09-09 21:29:27 +0300

    Structure and Loaders

---
+++ src/loader.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+
+class BasicLoader:
+    def __init__(self, path):
+        self.path = path
+
+    def get_data(self, extension):
+        return [file for file in os.listdir(self.path) if file.endswith(extension)]
+
+
+class CsvLoader(BasicLoader):
+    def processing(self):
+        csv_files = []
+        for csv in self.get_data('.csv'):
+            full_path = os.path.join(self.path, csv)
+            data = pd.read_csv(full_path)
+            csv_files.append(data)
+
+        return pd.concat(csv_files, ignore_index=True)
+
+
+class ExcelDataLoader(BasicLoader):
+    def processing(self, sheet_name=0):
+        excel_files = []
+        for file in self.get_data(('.xls', '.xlsx')):
+            full_path = os.path.join(self.path, file)
+            df = pd.read_excel(full_path, sheet_name=sheet_name)
+            excel_files.append(df)
+        return pd.concat(excel_files, ignore_index=True)
+
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# data_path = os.path.join(current_dir, '..', 'data', 'raw_data')
+# data_path = os.path.normpath(data_path)
+#
+# loader = CsvLoader(data_path)
+# print(loader.processing())
